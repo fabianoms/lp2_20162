b28937bff2285fc5f60a276b58e0fc68eaa6ec51
fabiano <fabs.cg@gmail.com>
2017-03-14 12:25:03 -0300

    Exemplo de ListIterator.

---
+++ LP2/src/iterators/Main.java
@@ -0,0 +1,36 @@
+package iterators;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> al = new ArrayList<String>();
+		List<Integer> ll = new LinkedList<Integer>();
+		
+		al.add("Daniel");
+		al.add("Fabiano");
+		al.add("Manoel");
+		al.add("Carlos");
+		al.add("José");
+		ll.add(1);
+		ll.add(11);
+		ll.add(21);
+		ll.add(34);
+		ll.add(45);
+		
+		
+		ListIterator iterator = ll.listIterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		while (iterator.hasPrevious()) {
+			System.out.println(iterator.previous());
+		}
+
+	}
+
+}
